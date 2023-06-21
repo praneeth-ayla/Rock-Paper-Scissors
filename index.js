2fbca8d8806b35051a3ad3bf10eb722398fa4610
praneeth-ayla <praneethayla@gmail.com>
2023-06-21 19:30:03 +0530

    game completed with basic ui

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ let player, computer, result;
 choiceBtns.forEach(button => button.addEventListener("click", () => {
       player = button.textContent;
       computer = computerTurn();
-      playerText.textContent = `Player: ${player}`;
-      computerText.textContent = `computer: ${computer}`;
-      resultText.textContent = singleRound(computer, player);
+      playerText.textContent = `Player: ${player.toUpperCase()}`;
+      computerText.textContent = `Computer: ${computer.toUpperCase()}`;
+      resultText.textContent = `Result: ${singleRound(computer, player)}`;
+      player.toLowerCase();
+      
       roundResult = singleRound(computer, player);
+
     }));
 
 
@@ -20,11 +23,11 @@ choiceBtns.forEach(button => button.addEventListener("click", () => {
 function computerTurn() {
     let computerInput = Math.floor((Math.random() * 3) + 1);
         if (computerInput == 1 ){
-            computerInput = "rock"
+            computerInput = "ROCK"
         } else if (computerInput == 2 ){
-        computerInput= "paper"
+        computerInput= "PAPER"
         }else if (computerInput == 3 ){
-            computerInput= "scissors"
+            computerInput= "SCISSORS"
         }
         return computerInput
     }
@@ -33,12 +36,29 @@ function singleRound(computerSelection, playerSelection) {
     if (computerSelection === playerSelection) {
         return "Tie!"
     } else if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "scissors" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "rock") 
+        (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+        (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
+        (playerSelection === "PAPER" && computerSelection === "ROCK") 
     ) {
-        return ("Won!");
+        return ("You Won!");
     } else {
-        return ("Lost!");
+        return ("You Lost!");
     }
 }
+
+
+
+// let ties, playerScore, computerScore;
+
+// while (true) {
+//     if (computerScore==5) {
+//         const gameOver = document.createElement("h1");
+//         gameOver.textContent = `You Lost! Computer Score: ${computerScore}, Your Score: ${playerScore}`;
+//     } else if (playerScore==5){
+//         const gameOver = document.createElement("h1");
+//         gameOver.textContent = `You Won! Your Score: ${playerScore}, Computer Score: ${computerScore}`;
+//     } else {
+//         singleRound(computer, player)
+//     }
+    
+// }
